6caf009657fc400e0ab37890c4f7e5533cef8a5d
Bercovici Adrian <bercovici.adrian@outlook.com>
2019-03-28 17:53:49 +0200

    group;s added

--- Monad.hs
+++ Monad.hs
@@ -6,3 +6,13 @@ module Monad where
     instance Functor Tree where
         fmap f (Leaf x)=Leaf (f x)
         fmap f (Node left right)=Node (fmap f left) (fmap f right)
+    
+
+    groups::[Int]->[[Int]]
+    groups ls=go ls [] [] where
+       
+        go (x:xs) [] bg= go xs [x] bg
+        go (x:xs) (y:ys) bg | x==y = go xs (x:y:ys) bg
+                            | otherwise = go xs [] ((y:ys):bg)
+        
+    
