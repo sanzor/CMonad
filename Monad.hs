f33b44f35aa2578790d66b30837920efcdfe6e39
Bercovici Adrian <bercovici.adrian@outlook.com>
2019-04-10 16:00:37 +0300

    started applicative for nodes

--- Monad.hs
+++ Monad.hs
@@ -12,15 +12,18 @@ module Monad where
     instance Applicative Tree where
         pure =Leaf
         (<*>) _ Empty =Empty
+        (<*>) (Node f1 f2) (Node v1 v2)=Node 
+                                          (Node (Leaf (f1 v1) Leaf (f1 vs))) (Node (Leaf f2 v1)(Leaf f2 v2))
         (<*>) (Leaf f) (Leaf x)=Leaf $ f x 
-        (<*>) (Node l r) (Leaf x)=Leaf fmap l r
+        (<*>) (Node l r) (Leaf x)=Leaf fmap (l.r) x
+        (<*>) 
         
-    aplicativeTest::Maybe String->IO (Maybe String)
-    aplicativeTest mstring=do
-       pure writeFile <*> mstring $ Just "asa"
-        
-            
-       return . Just  $ "Done"
+    -- aplicativeTest::Maybe String->IO (Maybe String)
+    -- aplicativeTest mstring=do
+    --    (pure writeFile) <*> mstring
+    --    return . Just  $ "Done"
+    
+    --IO (Maybe String)
 
         
 
