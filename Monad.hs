f6825799f99ed5a316292cfabd6c64564d34f9e0
sanzor <adriansan_93@yahoo.com>
2019-04-11 23:45:49 +0300

    eod commit

--- Monad.hs
+++ Monad.hs
@@ -1,30 +1,14 @@
 module Monad where
     import Control.Monad
     import Tree
-    import Control.Applicative
     
-    
+
     instance Functor Tree where
-        fmap _ Empty=Empty
+        fmap _  Empty=Empty
         fmap f (Leaf x)=Leaf (f x)
-        fmap f (Node left right)=Node (fmap f left) (fmap f right)
-    
-    instance Applicative Tree where
-        pure =Leaf
-        (<*>) _ Empty =Empty
-        (<*>) (Node f1 f2) (Node v1 v2)=Node 
-                                          (Node (Leaf (f1 v1) Leaf (f1 vs))) (Node (Leaf f2 v1)(Leaf f2 v2))
-        (<*>) (Leaf f) (Leaf x)=Leaf $ f x 
-        (<*>) (Node l r) (Leaf x)=Leaf fmap (l.r) x
-        (<*>) 
+        fmap f (Node  el left right)=Node (f el) left right
         
-    -- aplicativeTest::Maybe String->IO (Maybe String)
-    -- aplicativeTest mstring=do
-    --    (pure writeFile) <*> mstring
-    --    return . Just  $ "Done"
+    -- instance Applicative Tree where
+    --     pure 
     
-    --IO (Maybe String)
-
-        
-
     
