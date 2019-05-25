81f43c4f3ba8f3424b7fddde04dbec4686425712
sanzor <adriansan_93@yahoo.com>
2019-05-25 23:42:30 +0300

    add last commit b4 es

--- State.hs
+++ State.hs
@@ -1,35 +1,27 @@
-module State where 
-    import Control.Monad 
-    import System.Directory
-   
-    newtype State s a = State {run::s->(a,s)} 
+module State where
 
+    newtype State s a=State{run::s->(a,s)}
+    
+    
     instance Functor (State s) where
-        fmap=Control.Monad.liftM
+        fmap = Control.Monad.liftM
     instance Applicative (State s) where
         pure=return
         (<*>)=Control.Monad.ap
-    instance Monad (State s) where
-        return a= State $ \k->(a,k) 
-        (>>=) m f=State $ \s -> let (a,s')=run m s in
-            run (f a) s'
-    
 
+    instance Monad(State s) where
+        return a=State $ (\s->(a,s))
+        (>>=) m f=State $ (\s ->let (a,s')=run m s in
+                                           run (f a) s'
+  
+
+    
     get::State s s
-    get=State $ \s -> (s,s)
+    get=State $ \s ->(s,s)
 
-    put :: s -> State s ()
-    put s = State $ \_ -> ((), s)
-    
+    put::s->State s ()
+    put x=State $ \s ->((),s)
 
     modify::(s->s)->State s ()
-    modify f = get>>= \s -> put (f s) 
-
-    evalState::State s a ->s->a
-    evalState act=fst . run act
-
-    execState::State s a ->s->s
-    execState act=snd . run act
-
-    -- changeName::String->State Env ()
-    -- changeName str= State $ \(Env _ f)-> ((),Env str f )
+    modify f=get>>=\s -> put (f s)
+   
